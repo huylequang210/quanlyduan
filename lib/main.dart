68c7e8eebd058d9f1fb50e9d75459f1b555c2c8f
huylequang210 <58809807+huylequang210@users.noreply.github.com>
2021-12-23 17:56:28 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterFire Samples',
+      title: 'Quản lý dự án',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.indigo,
